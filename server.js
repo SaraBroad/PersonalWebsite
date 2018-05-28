7e6595516a16f54dbe52a02f040594889f8b415a
Sara Broad <saraebroad@gmail.com>
2018-05-27 22:34:06 -0400

    commit

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.unsubscribe(bodyParser.json());
+app.use(bodyParser.json());
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
 
   
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
