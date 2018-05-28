b0ad94bd378eda8a8c4b09a80a80e3a948944afb
Sara Broad <saraebroad@gmail.com>
2018-05-27 23:01:58 -0400

    formatting

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
+});
 
   
