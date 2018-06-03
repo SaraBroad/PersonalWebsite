b60b44962dfa870a987e0008f09e067cfa983f16
Sara Broad <saraebroad@gmail.com>
2018-06-03 16:45:53 -0400

    npm stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+// var exphbs = require("express-handlebars");
+var nodemailer = require("nodemailer");
 
 var app = express();
 var PORT = 8080;
@@ -9,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
 
 require("./routes/html-routes.js")(app);
 
